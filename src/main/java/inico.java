148aaebe7b07b80dc6fc026e32c7e933995ab99a
marigonzablanco <marigonzablanco@gmail.com>
2025-11-03 13:33:03 +0100

    desde formulariocliente

--- src/main/java/inico.java
+++ src/main/java/inico.java
@@ -2,7 +2,7 @@ public class inico {
     public static void main(String[] args) {
         System.out.println("hello");
 
-        System.out.println("otro");
-System.out.println("otro");
+        System.out.println("desde formulario cliente");
+
     }
 }
